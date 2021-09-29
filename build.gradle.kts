6247e597c46fb41e68b91a97ed09ab7111624d03
NightEule5 <24661563+NightEule5@users.noreply.github.com>
2021-09-28 22:47:44 -0600

    Set up the initial build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,71 @@
+
+import dev.strixpyrr.shorthand.CompilerArgumentScope.Companion.RequiresOptIn
+import dev.strixpyrr.shorthand.JvmDefaultMode.All
+import dev.strixpyrr.shorthand.freeCompilerArgs
+
 plugins {
 	kotlin("jvm") version "1.5.31"
+	id("dev.strixpyrr.shorthand")
+	`java-gradle-plugin`
 }
 
 group = "dev.strixpyrr.power-loom"
 version = "0.0.1"
 
-repositories {
+repositories()
+{
 	mavenCentral()
 }
 
-dependencies {
+dependencies()
+{
 	implementation(kotlin("stdlib"))
+	implementation(gradleApi())
+	
+	implementation(deps.okio)
+	implementation(deps.kotlinx.serialization.json)
+	
+	testImplementation(deps.kotest)
+}
+
+kotlin()
+{
+	target.run()
+	{
+		compilations.forEach()
+		{
+			it.kotlinOptions.run()
+			{
+				jvmTarget       = "1.8"
+				languageVersion = "1.5"
+				
+				freeCompilerArgs()
+				{
+					optIn(RequiresOptIn)
+					
+					jvmDefault = All
+				}
+			}
+		}
+	}
+}
+
+tasks()
+{
+	withType<Test>
+	{
+		useJUnitPlatform()
+	}
+}
+
+gradlePlugin()
+{
+	plugins()
+	{
+		val powerLoom by creating()
+		{
+			id = "dev.strixpyrr.power-loom"
+			implementationClass = "dev.strixpyrr.powerLoom.PowerLoomPlugin"
+		}
+	}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,28 @@
+enableFeaturePreview("VERSION_CATALOGS")
+
+pluginManagement()
+{
+	repositories()
+	{
+		gradlePluginPortal()
+		maven(url = "https://jitpack.io")
+	}
+	
+	plugins()
+	{
+		id("dev.strixpyrr.shorthand") version "0.0.3"
+	}
+}
+
+dependencyResolutionManagement()
+{
+	versionCatalogs()
+	{
+		val deps by creating()
+		{
+			from(files("versions.toml"))
+		}
+	}
+}
+
 rootProject.name = "power-loom"
